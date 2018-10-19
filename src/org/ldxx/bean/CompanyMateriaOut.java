c326d97a5c14704de9dc81ee9c0414c2f5d7b947
liuzhenfeng <liuzhenfeng@163.com>
2018-10-19 09:18:37 +0800

    lzf

--- src/org/ldxx/bean/CompanyMateriaOut.java
+++ src/org/ldxx/bean/CompanyMateriaOut.java
@@ -13,7 +13,7 @@ public class CompanyMateriaOut {
 	private String cgNo;//采购合同号
 	private String cgName;//采购合同名
 	private String taskNo;//任务单号
-	private String pickProject;//需求项目
+	private String pickProject;//领料项目
 	private String pickDate;//领料日期
 	private String pickPerson;//领料人
 	private String cmoDesc;//备注
@@ -24,8 +24,6 @@ public class CompanyMateriaOut {
 	private String remainType;//结余状态 0 未结余 1 已结余
 	private List<Accessory> accessory;
 	
-	private String uName; //人员名称
-
 	
 	public List<Accessory> getAccessory() {
 		return accessory;
@@ -139,12 +137,5 @@ public class CompanyMateriaOut {
 		this.remainType = remainType;
 	}
 
-	public String getuName() {
-		return uName;
-	}
-
-	public void setuName(String uName) {
-		this.uName = uName;
-	}
 	
 }
