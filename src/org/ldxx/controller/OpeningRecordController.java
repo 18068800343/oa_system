ccc8763e49abf1314526c1193afc291117feda25
liuzhenfeng <liuzhenfeng@163.com>
2018-11-27 09:16:29 +0800

    lzf

--- src/org/ldxx/controller/OpeningRecordController.java
+++ src/org/ldxx/controller/OpeningRecordController.java
@@ -1,9 +1,16 @@
 package org.ldxx.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.ldxx.bean.Accessory;
+import org.ldxx.bean.ManagingDocuments;
+import org.ldxx.bean.ManagingDocumentsTenderer;
+import org.ldxx.bean.OpeningInformation;
 import org.ldxx.bean.OpeningRecord;
 import org.ldxx.service.OpeningRecordService;
 import org.ldxx.util.TimeUUID;
@@ -11,7 +18,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import net.sf.json.JSONObject;
 
 /**
  * 开标记录
@@ -40,34 +51,110 @@ public class OpeningRecordController {
 	
 	@RequestMapping("/addOpeningRecordSave")
 	@ResponseBody
-	public Map<String,Object> addOpeningRecordSave(@RequestBody OpeningRecord record){
+	public Map<String,Object> addOpeningRecordSave(String record,@RequestParam MultipartFile [] file) throws IllegalStateException, IOException{
 		Map<String,Object> map = new HashMap<>();
+		Map<String,Class> map2=new HashMap<>();
+		map2.put("openingInformation", OpeningInformation.class);
+		JSONObject jsonObject=JSONObject.fromObject(record);
+		OpeningRecord rd=(OpeningRecord)JSONObject.toBean(jsonObject, OpeningRecord.class,map2);
 		TimeUUID uuid=new TimeUUID();
 		String id=uuid.getTimeUUID();
-		record.setOrId(id);
-		int i=service.addOpeningRecordSave(record);
+		rd.setOrId(id);
+		String path="D:"+File.separator+"oa"+File.separator+"OpeningRecord"+File.separator+id;;
+		File f=new File(path);
+		if(!f.exists()){
+			f.mkdirs();
+		}
+		if(file.length>0){
+			List<Accessory> list=new ArrayList<>();
+			for(int i=0;i<file.length;i++){
+				Accessory accessory=new Accessory();
+				String fileName=file[i].getOriginalFilename();
+				String filePath=path+File.separator+fileName;
+				File f1=new File(filePath);
+				file[i].transferTo(f1);
+				accessory.setaId(id);
+				accessory.setAcName(fileName);
+				accessory.setAcUrl(filePath);
+				accessory.setaType("开标记录公示截图");
+				list.add(accessory);
+			}
+			rd.setAccessory(list);
+		}
+		int i=service.addOpeningRecordSave(rd);
 		map.put("result", i);
-		map.put("OpeningRecord", record);
+		map.put("OpeningRecord", rd);
 		return map;
 	}
 	@RequestMapping("/addOpeningRecordSubmit")
 	@ResponseBody
-	public Map<String,Object> addOpeningRecordSubmit(@RequestBody OpeningRecord record){
+	public Map<String,Object> addOpeningRecordSubmit(String record,@RequestParam MultipartFile [] file) throws IllegalStateException, IOException{
 		Map<String,Object> map = new HashMap<>();
+		Map<String,Class> map2=new HashMap<>();
+		map2.put("openingInformation", OpeningInformation.class);
+		JSONObject jsonObject=JSONObject.fromObject(record);
+		OpeningRecord rd=(OpeningRecord)JSONObject.toBean(jsonObject, OpeningRecord.class,map2);
 		TimeUUID uuid=new TimeUUID();
 		String id=uuid.getTimeUUID();
-		record.setOrId(id);
-		int i=service.addOpeningRecordSave(record);
+		rd.setOrId(id);
+		String path="D:"+File.separator+"oa"+File.separator+"OpeningRecord"+File.separator+id;;
+		File f=new File(path);
+		if(!f.exists()){
+			f.mkdirs();
+		}
+		if(file.length>0){
+			List<Accessory> list=new ArrayList<>();
+			for(int i=0;i<file.length;i++){
+				Accessory accessory=new Accessory();
+				String fileName=file[i].getOriginalFilename();
+				String filePath=path+File.separator+fileName;
+				File f1=new File(filePath);
+				file[i].transferTo(f1);
+				accessory.setaId(id);
+				accessory.setAcName(fileName);
+				accessory.setAcUrl(filePath);
+				accessory.setaType("开标记录公示截图");
+				list.add(accessory);
+			}
+			rd.setAccessory(list);
+		}
+		int i=service.addOpeningRecordSave(rd);
 		map.put("result", i);
-		map.put("OpeningRecord", record);
+		map.put("OpeningRecord", rd);
 		return map;
 	}
 	
 	@RequestMapping("/updateOpeningRecordSave")
 	@ResponseBody
-	public Map<String,Object> updateOpeningRecordSave(@RequestBody OpeningRecord record){
+	public Map<String,Object> updateOpeningRecordSave(String record,@RequestParam MultipartFile [] file) throws IllegalStateException, IOException{
 		Map<String,Object> map = new HashMap<>();
-		int i=service.updateOpeningRecordSave(record);
+		Map<String,Class> map2=new HashMap<>();
+		map2.put("openingInformation", OpeningInformation.class);
+		JSONObject jsonObject=JSONObject.fromObject(record);
+		OpeningRecord rd=(OpeningRecord)JSONObject.toBean(jsonObject, OpeningRecord.class,map2);
+		String id=rd.getOrId();
+		String path="D:"+File.separator+"oa"+File.separator+"OpeningRecord"+File.separator+id;;
+		File f=new File(path);
+		if(!f.exists()){
+			f.mkdirs();
+		}
+		if(file.length>0){
+			List<Accessory> list=new ArrayList<>();
+			for(int i=0;i<file.length;i++){
+				Accessory accessory=new Accessory();
+				String fileName=file[i].getOriginalFilename();
+				String filePath=path+File.separator+fileName;
+				File f1=new File(filePath);
+				file[i].transferTo(f1);
+				accessory.setaId(id);
+				accessory.setAcName(fileName);
+				accessory.setAcUrl(filePath);
+				accessory.setaType("开标记录公示截图");
+				list.add(accessory);
+			}
+			rd.setAccessory(list);
+		}
+		int i=service.updateOpeningRecordSave(rd);
 		map.put("result", i);
 		map.put("OpeningRecord", record);
 		return map;
@@ -75,9 +162,35 @@ public class OpeningRecordController {
 	
 	@RequestMapping("/updateOpeningRecordSubmit")
 	@ResponseBody
-	public Map<String,Object> updateOpeningRecordSubmit(@RequestBody OpeningRecord record){
+	public Map<String,Object> updateOpeningRecordSubmit(String record,@RequestParam MultipartFile [] file) throws IllegalStateException, IOException{
 		Map<String,Object> map = new HashMap<>();
-		int i=service.updateOpeningRecordSave(record);
+		Map<String,Class> map2=new HashMap<>();
+		map2.put("openingInformation", OpeningInformation.class);
+		JSONObject jsonObject=JSONObject.fromObject(record);
+		OpeningRecord rd=(OpeningRecord)JSONObject.toBean(jsonObject, OpeningRecord.class,map2);
+		String id=rd.getOrId();
+		String path="D:"+File.separator+"oa"+File.separator+"OpeningRecord"+File.separator+id;;
+		File f=new File(path);
+		if(!f.exists()){
+			f.mkdirs();
+		}
+		if(file.length>0){
+			List<Accessory> list=new ArrayList<>();
+			for(int i=0;i<file.length;i++){
+				Accessory accessory=new Accessory();
+				String fileName=file[i].getOriginalFilename();
+				String filePath=path+File.separator+fileName;
+				File f1=new File(filePath);
+				file[i].transferTo(f1);
+				accessory.setaId(id);
+				accessory.setAcName(fileName);
+				accessory.setAcUrl(filePath);
+				accessory.setaType("开标记录公示截图");
+				list.add(accessory);
+			}
+			rd.setAccessory(list);
+		}
+		int i=service.updateOpeningRecordSave(rd);
 		map.put("result", i);
 		map.put("OpeningRecord", record);
 		return map;
