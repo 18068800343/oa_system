9fb3c348fe505a2ba04fd6f8958291c03c49b07f
Jack <1136329229@qq.com>
2019-01-24 16:10:39 +0800

    zhaohui

---
+++ src/org/ldxx/bean/LianYingHeSuan.java
@@ -0,0 +1,65 @@
+package org.ldxx.bean;
+
+/** 
+* @ClassName: LianYingHeSuan 
+* @Description: 联营合算
+* @author zh
+* @date 2019年1月24日 上午11:38:19 
+*  
+*/
+public class LianYingHeSuan {
+
+	private String prjName;
+	private String contractNo;
+	private String contractMoney;
+	private String endMoney;
+	private String lyUnitName;
+	private String lyNo;
+	private String managementFee;
+	
+	
+	
+	public String getPrjName() {
+		return prjName;
+	}
+	public void setPrjName(String prjName) {
+		this.prjName = prjName;
+	}
+	public String getContractNo() {
+		return contractNo;
+	}
+	public void setContractNo(String contractNo) {
+		this.contractNo = contractNo;
+	}
+	public String getContractMoney() {
+		return contractMoney;
+	}
+	public void setContractMoney(String contractMoney) {
+		this.contractMoney = contractMoney;
+	}
+	public String getEndMoney() {
+		return endMoney;
+	}
+	public void setEndMoney(String endMoney) {
+		this.endMoney = endMoney;
+	}
+	public String getLyUnitName() {
+		return lyUnitName;
+	}
+	public void setLyUnitName(String lyUnitName) {
+		this.lyUnitName = lyUnitName;
+	}
+	public String getLyNo() {
+		return lyNo;
+	}
+	public void setLyNo(String lyNo) {
+		this.lyNo = lyNo;
+	}
+	public String getManagementFee() {
+		return managementFee;
+	}
+	public void setManagementFee(String managementFee) {
+		this.managementFee = managementFee;
+	}
+	
+}
