1c582c2df077330fb0d95f136df8b7dc44d3b862
Jack <1136329229@qq.com>
2019-01-28 17:26:47 +0800

    zhaohui

--- src/org/ldxx/dao/LianYingDao.java
+++ src/org/ldxx/dao/LianYingDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 import org.ldxx.bean.LianYing;
+import org.ldxx.bean.LianYingHeSuan;
 
 public interface LianYingDao {
 	
@@ -16,5 +17,9 @@ public interface LianYingDao {
 	public int lyNoCount(@Param("year")String year);
 	
 	public LianYing selectLianYingByNo(@Param("no")String no);
+	
+	public int addLianYingHeSuan(@Param("hs")LianYingHeSuan hs);
+	
+	public List<LianYingHeSuan> selectLianYingHeSuan();
 
 }
