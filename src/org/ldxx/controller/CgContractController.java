918eff2c546e61c6d75fd4244d85e8c294c8d0e7
liuzhenfeng <liuzhenfeng@163.com>
2019-02-28 14:27:11 +0800

    lzf

--- src/org/ldxx/controller/CgContractController.java
+++ src/org/ldxx/controller/CgContractController.java
@@ -749,11 +749,8 @@ public class CgContractController {
 		}*/
 		int i=cgService.updateCgContractById(cg);
 		if(i>0){
-			User user = (User) session.getAttribute("user");
 			String departName = cg.getCgDepartment();
 			OrganizationManagement oManagement = oService.getOrgIdByName(departName);
-			
-			//OrganizationManagement om=oService.selectOrgById(user.getOmId());
 			String omNo=oManagement.getOmNo();
 			currentFlowMapper.updateFkDeptByModeId(cg.getCgId(), omNo);
 		}
