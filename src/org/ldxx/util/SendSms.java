d5f2a030aca120943d8afb98a27210c533315d62
Jack <1136329229@qq.com>
2019-03-22 09:57:14 +0800

    zhaohui

---
+++ src/org/ldxx/util/SendSms.java
@@ -0,0 +1,21 @@
+package org.ldxx.util;
+import rtx.RTXSvrApi;   
+public class SendSms{
+    
+	public int sendMessage(String receivers,String title,String msg,String type,String delayTime){
+		int iRet= -1;
+    	RTXSvrApi  RtxsvrapiObj = new RTXSvrApi();   
+    	if( RtxsvrapiObj.Init()){
+        		iRet = RtxsvrapiObj.sendNotify(receivers,title,msg, type,delayTime);
+        		if (iRet == 0){
+        			System.out.println("成功");
+        		}else{
+        			System.out.println("失败");
+        		}
+
+        	}
+        	RtxsvrapiObj.UnInit();
+        	return iRet;
+	}
+	
+}
