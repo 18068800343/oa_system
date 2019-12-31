b58de93221da7d4ffe0e0dd19cbe7ecb1e589021
liuzhenfeng <liuzhenfeng@163.com>
2019-12-31 15:51:46 +0800

    lzf

--- WebRoot/view/js/quanxian.js
+++ WebRoot/view/js/quanxian.js
@@ -7,37 +7,38 @@
 		},
 		success:function(json){
 			if(json!=null &&json!=""){
-			var arr = json.uPermissions.split(',');
-			console.log(arr);
-				$.ajax({
-					type:"post",
-					url:getContextPath()+"/permissions/getAllPermissions",
-					data:{
-					},
+			var arr;
+			var arr2 =[];
+			if(json.uPermissions!=null&&json.uPermissions!=""){
+				arr = json.uPermissions.split(',');
+			}
+			$.ajax({
+				type:"post",
+				url:getContextPath()+"/permissions/getAllPermissions",
+				data:{},
 				success:function(json2){
-					var arr2 =[];
+					
 					for(var a=0;a<json2.list.length;a++){
 						arr2.push(json2.list[a].coding)
 					}
-					for(var i=0;i<arr.length;i++){
-						userbm=arr[i];
+					if(arr!=null&&arr!=""){
+						for(var i=0;i<arr.length;i++){
+							userbm=arr[i];
 							for(var j=0;j<arr2.length;j++){
 								var qbbm=arr2[j]
 								if(qbbm.indexOf(userbm)>=0){
 									var c=$.inArray(userbm,arr2);
 									arr2.splice($.inArray(userbm,arr2),1);
 								}
-                            
+								
+							}
 						}
+						
 					}
 						//要删除的集合
-					console.log(arr2)	
+					//console.log(arr2)	
 						for(var j=0;j<arr2.length;j++){
-						
 									var arr4 = arr2[j].replace("+","");
-									if(arr4=="Pab"){
-										console.log(arr4)
-									}
 									$("."+arr4+"").hide();
 									$("."+arr4+"").closest("li").remove();//通过编码 点 remove
 									
